876e3092bbedf4f2a1c301498e17c02be692e7bb
ZenoHwanEth <hwan2221@hotmail.com>
2023-03-26 16:34:43 +0800

    remove isMount

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,14 +12,13 @@ const Tech = () => {
 
   return (
     <>
-    {isMounted && (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
            <BallCanvas icon={technology.icon} /> 
         </div>
       ))}
-    </div> )}
+    </div> 
     </>
   );
 };
