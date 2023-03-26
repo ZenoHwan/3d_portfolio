dff508bb3d23cccc457c0666fac6bc87d7618532
ZenoHwanEth <hwan2221@hotmail.com>
2023-03-26 22:53:18 +0800

    testing on mount for tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,28 @@
-import React, { lazy } from "react";
+import React, { lazy, useState, useEffect} from "react";
 const  BallCanvas  = lazy(() => import('../components/canvas/Ball'));
 
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsMounted(true)  
+    }, 2000);
+  }, []);
+
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-           <BallCanvas icon={technology.icon} /> 
-        </div>
-      ))}
-    </div> 
+    <>
+      {isMounted && (
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} /> 
+          </div>
+        ))}
+      </div> )}
+    </>
   );
 };
 
