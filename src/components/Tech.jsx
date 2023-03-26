c8e5ae6a30a952c596f11b23a1d9dc5f69ea2996
ZenoHwanEth <hwan2221@hotmail.com>
2023-03-26 16:26:07 +0800

    change back ballcanvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,20 +1,15 @@
-import React, { lazy, useState, useEffect} from "react";
-const  BallCanvas  = lazy(() => import('../components/canvas/Ball'));
+import React from "react";
 
+import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          {isMounted && <BallCanvas icon={technology.icon} /> }
+          <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
