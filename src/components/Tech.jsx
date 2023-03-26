1486306f68913614b97cfeb96c562f0a1e531620
ZenoHwanEth <hwan2221@hotmail.com>
2023-03-26 16:28:26 +0800

    adding isMount for whole section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { lazy, useState, useEffect} from "react";
+const  BallCanvas  = lazy(() => import('../components/canvas/Ball'));
 
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
+    <>
+    {isMounted && (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+           <BallCanvas icon={technology.icon} /> 
         </div>
       ))}
-    </div>
+    </div> )}
+    </>
   );
 };
 
