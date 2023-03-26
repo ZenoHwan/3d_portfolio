b6a73b0a1189ae6b6a4c07cebfa70d1d8954e0aa
ZenoHwanEth <hwan2221@hotmail.com>
2023-03-26 22:56:38 +0800

    changing tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,28 +1,18 @@
-import React, { lazy, useState, useEffect} from "react";
-const  BallCanvas  = lazy(() => import('../components/canvas/Ball'));
+import React from "react";
 
+import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsMounted(true)  
-    }, 2000);
-  }, []);
-
   return (
-    <>
-      {isMounted && (
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologies.map((technology) => (
-          <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon} /> 
-          </div>
-        ))}
-      </div> )}
-    </>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   );
 };
 
